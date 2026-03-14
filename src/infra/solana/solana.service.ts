708fe2dcd757cdb8c950c5ba7745884d24921e0d
NgocDang <ngocdang240604@gmail.com>
2026-03-14 14:29:36 +0700

    refactor: remove unused getProgramId method from SolanaService

--- src/infra/solana/solana.service.ts
+++ src/infra/solana/solana.service.ts
@@ -59,10 +59,6 @@ export class SolanaService {
     return this.network === 'devnet' ? 'https://api-devnet.helius.xyz' : 'https://api.helius.xyz';
   }
 
-  getProgramId(): PublicKey | undefined {
-    return this.programId;
-  }
-
   async getBalance(publicKey: PublicKey, useHelius = false): Promise<number> {
     const conn = useHelius ? this.heliusConnection : this.connection;
     try {
