a36c335f2f07d5e48fdc729b8808aa440c162da6
Ngoc Le <ngoc.le@sap.com>
2026-05-21 10:49:06 +0700

    feat: DI for executor service, prepare for Intergrating with solsight-executor service

--- src/modules/swap/interfaces/swap.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { JupiterQuoteResponse, JupiterSwapResponse } from "src/infra/jupiter/types";
-
-export interface QuoteResponse extends JupiterQuoteResponse {}
-export interface SwapResponse extends JupiterSwapResponse {}
