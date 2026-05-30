5696c7561aa79b5ea8669a317b28113f1c9577f4
Ngoc Le <ngocle266tqt@gmail.com>
2026-05-30 18:02:36 +0700

    chore: remove unused token echo controller

--- src/common/cluster/cluster.module.ts
+++ src/common/cluster/cluster.module.ts
@@ -3,12 +3,10 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
 import { ConfigModule } from "@nestjs/config";
 import { ClusterProvider } from "./cluster.provider";
 import { ClusterInterceptor } from "./cluster.interceptor";
-import { ClusterEchoController } from "./cluster-echo.controller";
 
 @Global()
 @Module({
     imports: [ConfigModule],
-    controllers: [ClusterEchoController],
     providers: [
         ClusterProvider,
         {
