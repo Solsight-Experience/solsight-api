3c2a3b62d7ea2e6c14d82c3f6dcfb9635ceb6b68
Vo Van Nam <vovannam050204@gmail.com>
2026-06-24 23:15:40 +0700

    fix(SCRUM-245): sync favorite state between discovery and detail pages

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Token } from "../tokens/entities/token.entity";
 import { AccountController } from "./controllers/account.controller";
 import { AccountService } from "./services/account.service";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Token])],
     controllers: [AccountController],
     providers: [AccountService]
 })
