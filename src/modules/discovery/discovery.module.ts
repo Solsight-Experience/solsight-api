f78616dc6e786c8b84ea1ba84a6c9516af295865
Nguyễn Ngọc Đăng <ngocdang240604@gmail.com>
2026-06-29 22:53:06 +0700

    Fix/[SCRUM-267] discovery time range filter (#94)
    * fix: time frame on token route and get trending route
    
    * fix: error lint
    
    * chore: add comment for resolveOhlcInterval function

--- src/modules/discovery/discovery.module.ts
+++ src/modules/discovery/discovery.module.ts
@@ -2,6 +2,8 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Token } from "../tokens/entities/token.entity";
 import { Category } from "../tokens/entities/category.entity";
+import { OhlcCandle } from "../tokens/entities/ohlc-candle.entity";
+import { Transaction } from "../transactions/entities/transaction.entity";
 import { DiscoveryController } from "./controllers/discovery.controller";
 import { DiscoveryService } from "./services/discovery.service";
 import { JupiterModule } from "../../infra/jupiter/jupiter.module";
@@ -9,7 +11,7 @@ import { CoinGeckoModule } from "../../infra/coingecko/coingecko.module";
 import { RedisModule } from "../../redis";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Token, Category]), JupiterModule, CoinGeckoModule, RedisModule],
+    imports: [TypeOrmModule.forFeature([Token, Category, OhlcCandle, Transaction]), JupiterModule, CoinGeckoModule, RedisModule],
     controllers: [DiscoveryController],
     providers: [DiscoveryService],
     exports: [DiscoveryService]
