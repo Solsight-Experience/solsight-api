7bff818f98c804f7d2e5ef8d68cb28b0dd7945a5
Ngoc Le <ngocle266tqt@gmail.com>
2026-07-08 21:09:41 +0700

    chore: add labels for holder accounts

--- src/modules/tokens/data/wallet-labels.ts
+++ src/modules/tokens/data/wallet-labels.ts
@@ -10,9 +10,18 @@ export const WALLET_LABELS: Record<string, { name: string; type: HolderAccountTy
     "5VCwKtCXgCJ6kit5FybXjvriW3xELsFDhYrPSqtJNmcD": { name: "OKX", type: "CEX" },
     // Changelly
     "3yFwqXBfZY4jBVUafQ1YEXw189y2dN3V5KQq9uzBDy1E": { name: "Changelly", type: "CEX" },
+    // Kraken
+    "2ojv9BAiHUrvsm9gxDe7fJSzbNZSJcxZvf8dqmWGHG8S": { name: "Kraken", type: "CEX" },
+    // Bybit
+    A7eHSuGAUCzDHMnJVVSBjgSWVFnqFHBVv7MBqFSmMNT4: { name: "Bybit", type: "CEX" },
+    // Bitget
+    C6tz7SZfaWxrnBNUrNkQ8NKJGRz5Q4pzFJAiEEQpjFja: { name: "Bitget", type: "CEX" },
+    // KuCoin
+    BmFdpraQhkiDQE6SnfG5omcA1VwzqfXrwtNYBwWTymy6: { name: "KuCoin", type: "CEX" },
+    // MEXC
+    BVxyYhm498L79r4HMQ9sxZ5bi41DmJmeWZ7SCS7Cyvna: { name: "MEXC", type: "CEX" },
     // Burn addresses
     "1nc1nerator11111111111111111111111111111111": { name: "Burn", type: "BURN" }
-    // Add more known wallets...
 };
 
 export function getWalletLabel(address: string): { name: string; type: HolderAccountType } | null {
